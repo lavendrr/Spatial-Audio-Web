60075964b45b85f4a04078cd66b68a37ade983d0
r-moctezuma <roberto@fractalriver.com>
2024-04-04 23:59:25 -0500

    Added Dockerfile w/nginx

---
+++ html/scripts.js
@@ -0,0 +1,24 @@
+
+const audioContext = new AudioContext();
+
+audioContext.onstatechange = () => console.log(audioContext.state); // running
+
+
+const audioElement = document.querySelector("audio");
+
+
+const track = audioContext.createMediaElementSource(audioElement);
+
+track.connect(audioContext.destination);
+
+// Event listener for the play button
+document.getElementById('playButton').addEventListener('click', function() {
+
+  if (audioContext.state === "suspended")
+  {
+    audioContext.resume();
+  }
+  audioElement.play();
+
+});
+
